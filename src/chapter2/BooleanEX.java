5c61e3d1372bad5deffe74b37277330885431f63
doccomsa <newcomsa@nate.com>
2024-03-19 17:00:26 +0900

    수업실습예제

---
+++ src/chapter2/BooleanEX.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class BooleanEX {
+
+	public static void main(String[] args) {
+		// 논리형데이타타입.  boolean.  true of false 값 사용목적.
+		
+		boolean isMarried = true;
+		System.out.println(isMarried);
+
+	}
+
+}
