b260d48016325be826a423ca5d61e0b0f520917f
doccomsa <newcomsa@nate.com>
2024-03-27 17:33:51 +0900

    추상클래스, 인터페이스, 다형성

---
+++ src/chapter8/inheritance2/Dog.java
@@ -0,0 +1,16 @@
+package chapter8.inheritance2;
+
+public class Dog extends Animal {
+
+	@Override
+	void run() {
+		System.out.println("강아지가 달린다.");
+	}
+
+	@Override
+	void cry() {
+		System.out.println("강아지가 멍멍 소리짖다.");
+	}
+
+	
+}
---
+++ src/chapter9/abstractex/Lion.java
@@ -0,0 +1,17 @@
+package chapter9.abstractex;
+
+public class Lion extends Animal {
+
+	@Override
+	void run() {
+		System.out.println("사자가 달린다.");
+
+	}
+
+	@Override
+	void cry() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
