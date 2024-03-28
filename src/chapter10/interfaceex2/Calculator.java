4e9dea0072caecb2aec1e33e8daf590973149831
doccomsa <newcomsa@nate.com>
2024-03-28 17:40:27 +0900

    인터페이스와 다형성, JDK기본기능

---
+++ src/chapter10/interfaceex2/Calculator.java
@@ -0,0 +1,23 @@
+package chapter10.interfaceex2;
+
+// 추상클래스 : 상속목적으로 설계. 객체생성 불가능
+public abstract class Calculator implements Calc {
+
+	//구현되지 않은 추상메서드를 2개상속받은 상태
+	
+	@Override
+	public int add(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1 + num2;
+	}
+
+	@Override
+	public int substract(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1 - num2;
+	}
+	
+	// 새로 추가작업 가능.
+
+	
+}
